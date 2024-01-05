3bf7f7929c54a7e272902e6fc6dc6e0852dfcd9c
benjjo <benjjo@gmail.com>
2024-01-05 15:58:39 +1100

    Friedman introduced.

--- prisoners_dilema.py
+++ prisoners_dilema.py
@@ -17,7 +17,7 @@ class GameRunner:
 
         for _ in range(self.num_games):
             # The 1% chance that noise is introduced
-            if random.randint(1, 100) != 1:
+            if random.randint(1, 50) != 1:
                 choice1 = self.player1.get_choice()
                 choice2 = self.player2.get_choice()
             else:
@@ -38,7 +38,7 @@ class GameRunner:
             # Update the strategies with the opponent's previous choice
             self.player1.make_choice(choice2)
             self.player2.make_choice(choice1)
-            print(f"{self.player1.name} = {choice1}\t\t {self.player2.name} = {choice2}")
+            print(f"{self.player1.name} = {choice1}\t\t\t\t {self.player2.name} = {choice2}")
 
         print(f"{self.player1.name}'s score: {player1_score}")
         print(f"{self.player2.name}'s score: {player2_score}")
@@ -141,14 +141,45 @@ class SoftTitForTat:
             self.set_choice("Defect")
 
 
+class Friedman:
+    """The strategy of do nice unto others until betrayed. This strategy is not a push-over.
+     A 'NICE' strategy in that we start peacefully until provoked. Once provoked it is unforgiving"""
+
+    def __init__(self, name="Friedman"):
+        self.name = name
+        self.choice = "Cooperate"
+        self.historic_opponent_choices = list()
+
+    def get_choice(self):
+        return self.choice
+
+    def set_choice(self, new_choice):
+        self.update_opp_choice(new_choice)
+        self.choice = new_choice
+
+    def update_opp_choice(self, recent_choice):
+        self.historic_opponent_choices.append(recent_choice)
+
+    def make_choice(self, opp_choice):
+        if 'Defect' in self.historic_opponent_choices:
+            self.set_choice("Defect")
+        elif opp_choice == "Cooperate":
+            self.set_choice("Cooperate")
+        else:
+            self.set_choice("Defect")
+
+
 # Example usage:
 tit_for_tat_strategy = TitForTat()
 soft_tit_for_tat_strategy = SoftTitForTat()
 always_defect_strategy = AlwaysDefect()
 always_cooperate_strategy = AlwaysCooperate()
+friedman_strategy = Friedman()
 
-# game_runner = GameRunner(tit_for_tat_strategy, always_defect_strategy, num_games=100)
-game_runner = GameRunner(soft_tit_for_tat_strategy, always_defect_strategy, num_games=100)
-# game_runner = GameRunner(tit_for_tat_strategy, always_cooperate_strategy, num_games=10)
-# game_runner = GameRunner(tit_for_tat_strategy, tit_for_tat_strategy, num_games=10)
+# game_runner = GameRunner(tit_for_tat_strategy, always_defect_strategy, num_games=200)
+# game_runner = GameRunner(soft_tit_for_tat_strategy, always_defect_strategy, num_games=200)
+# game_runner = GameRunner(tit_for_tat_strategy, always_cooperate_strategy, num_games=200)
+# game_runner = GameRunner(tit_for_tat_strategy, tit_for_tat_strategy, num_games=200)
+game_runner = GameRunner(soft_tit_for_tat_strategy, friedman_strategy, num_games=200)
+
 game_runner.run_game()
